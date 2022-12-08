4eda7672a95eef2b5c74ce3f9874081d93138831
Vikas_99 <vikas.prashar@precisely.com>
2022-12-08 20:54:33 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'quiz-test', component: QuizComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
