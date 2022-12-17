598d5e39bbff66ff423dff2d2924a31fb561642f
Vikas_99 <vikas.prashar@precisely.com>
2022-12-17 15:33:13 +0530

    console err fixed

--- src/app/resultmodal/resultmodal.component.ts
+++ src/app/resultmodal/resultmodal.component.ts
@@ -1,27 +1,26 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-resultmodal',
   templateUrl: './resultmodal.component.html',
-  styleUrls: ['./resultmodal.component.css']
+  styleUrls: ['./resultmodal.component.css'],
 })
 export class ResultmodalComponent implements OnInit {
-
   score: string | any;
   total: string | any;
-  constructor( public dialogRef: MatDialogRef<ResultmodalComponent>,   @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    public dialogRef: MatDialogRef<ResultmodalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.data.total)
-     this.score = this.data.score;
-     this.total = this.data.total;
+    this.score = this.data?.score;
+    this.total = this.data?.total;
   }
-  
 
-close() {
+  close() {
     this.dialogRef.close();
-    
-}
+  }
 }
